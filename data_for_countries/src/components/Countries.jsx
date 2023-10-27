58a19b730679081961c8b5725b339584a07cd138
ydaniju <yusuf.daniju@doctolib.com>
2023-10-27 19:15:25 +0200

    mock axios call

--- data_for_countries/src/components/Countries.jsx
+++ data_for_countries/src/components/Countries.jsx
@@ -5,20 +5,17 @@ import "./Countries.css";
 
 const Countries = ({ country, defaultShow = false }) => {
   const [show, setShow] = useState(false);
-  const [weathers, setweathers] = useState({});
+  const [weathers, setWeathers] = useState({});
   const countryCapital = country.capital[0];
 
   useEffect(() => {
-    // axios
-    //   .get(
-    //     `${Api}/current?access_key=f2585cd8464830c185d225a7ff49012e&query=${country.capital[0]}`
-    //   )
-    //   .then((response) => {
-    //     console.log(response.data);
-    //     setweathers(response.data);
-    //   });
-    // setweathers()
-  }, [countryCapital]);
+    axios
+      .get(
+        `${Api}/current?access_key=f2585cd8464830c185d225a7ff49012e&query=${country.capital[0]}`
+      )
+      .then(response => setWeathers(response))
+      .catch(console.error);
+  }, [countryCapital, setWeathers]);
 
   const handleShow = () => {
     setShow(!show);
@@ -28,41 +25,36 @@ const Countries = ({ country, defaultShow = false }) => {
     <div className="container">
       {show || defaultShow ? (
         <div>
-          <p className="country">
-            {country["name"].common}
-          </p>
-
-          <p className="capital">
-            {country["capital"][0]}
-          </p>
-
-          <p className="for_country">
-            {country["area"]}
-          </p>
-
-          <p className="for_country" data-testid='languages'>
+          <p className="country">{country["name"].common}</p>
+          <p className="capital">{country["capital"][0]}</p>
+          <p className="for_country">{country["area"]}</p>
+          <p className="for_country" data-testid="languages">
             {typeof country.languages === "object"
               ? Object.values(country["languages"]).map((val) => val)
               : country.languages}
           </p>
-          <p data-testid='flatest'>{country["flag"]}</p>
+          <p data-testid="flatest">{country["flag"]}</p>
           weather
-          {weathers.current && (
+          {weathers?.current && (
             <>
-              <p data-testid='wetest'>
-                {weathers.current.temperature}oC
-              </p>
-              <img data-testid='imatest' src={weathers.current.weather_icons[0]} alt="weather icon" />
+              <p data-testid="wetest">{`${weathers.current.temperature}oC`}</p>
+              <img
+                data-testid="imatest"
+                src={weathers.current.weather_icons[0]}
+                alt="weather icon"
+              />
             </>
           )}
         </div>
       ) : (
         <div>
           {" "}
-          <span data-testid='common'>
-            {country["name"].common}
-          </span>
-          <button data-testid='button' className="btn" onClick={() => handleShow()}>
+          <span data-testid="common">{country["name"].common}</span>
+          <button
+            data-testid="button"
+            className="btn"
+            onClick={() => handleShow()}
+          >
             show
           </button>
         </div>
--- data_for_countries/src/components/Countries.test.jsx
+++ data_for_countries/src/components/Countries.test.jsx
@@ -1,15 +1,64 @@
 import React from "react";
 import Countries from "./Countries";
-import { render, wait, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import { describe, vi } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { beforeEach, describe, vi } from "vitest";
 import "@testing-library/jest-dom";
+import axios from "axios";
 
 vi.mock("../utilities/Api.jsx");
+vi.mock("axios");
+
+const mockedAxios = vi.mocked(axios);
 
 describe("Countries", () => {
+  beforeEach(() => {
+    mockedAxios.get.mockResolvedValue({
+      request: {
+        type: "City",
+        query: "Mogadishu, Somalia",
+        language: "en",
+        unit: "m",
+      },
+      location: {
+        name: "Mogadishu",
+        country: "Somalia",
+        region: "Banaadir",
+        lat: "2.067",
+        lon: "45.367",
+        timezone_id: "Africa/Mogadishu",
+        localtime: "2023-10-27 19:51",
+        localtime_epoch: 1698436260,
+        utc_offset: "3.0",
+      },
+      current: {
+        observation_time: "04:51 PM",
+        temperature: 27,
+        weather_code: 116,
+        weather_icons: [
+          "https://cdn.worldweatheronline.com/images/wsymbols01_png_64/wsymbol_0004_black_low_cloud.png",
+        ],
+        weather_descriptions: ["Partly cloudy"],
+        wind_speed: 17,
+        wind_degree: 131,
+        wind_dir: "SE",
+        pressure: 1011,
+        precip: 0,
+        humidity: 79,
+        cloudcover: 29,
+        feelslike: 31,
+        uv_index: 1,
+        visibility: 10,
+        is_day: "no",
+      },
+    });
+  });
+
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
   describe("when defaultShow is false", () => {
-    it("should display the name of the country", () => {
+    it("should display the name of the country", async () => {
       render(
         <Countries
           defaultShow={false}
@@ -17,7 +66,7 @@ describe("Countries", () => {
         />
       );
 
-      expect(screen.getByText("Somalia")).toBeInTheDocument();
+      expect(await screen.findByText("Somalia")).toBeInTheDocument();
     });
 
     it("should not display the capital of the country", () => {
@@ -33,7 +82,7 @@ describe("Countries", () => {
   });
 
   describe("when defaultShow is true", () => {
-    it("should display the name of the country", () => {
+    it("should display the name of the country", async () => {
       render(
         <Countries
           defaultShow={true}
@@ -41,7 +90,7 @@ describe("Countries", () => {
         />
       );
 
-      expect(screen.getByText("Somalia")).toBeInTheDocument();
+      expect(await screen.findByText("Somalia")).toBeInTheDocument();
     });
 
     it("should not display the capital of the country", () => {
@@ -53,6 +102,17 @@ describe("Countries", () => {
       );
 
       expect(screen.queryByText("Mogadishu")).toBeInTheDocument();
+    });
+
+    it("should not display the capital of the country", async () => {
+      render(
+        <Countries
+          defaultShow={true}
+          country={{ capital: ["Mogadishu"], name: { common: "Somalia" } }}
+        />
+      );
+
+      expect(await screen.findByText("27oC")).toBeInTheDocument();
     });
   });
 });
